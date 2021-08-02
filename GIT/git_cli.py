3aaafeefd50fe022020189d7603f07e1ad23e953
Vadim Volynkin <71ooo@mail.ru>
2021-08-02 23:30:30 +0300

    updated git_cli

--- GIT/git_cli.py
+++ GIT/git_cli.py
@@ -8,12 +8,11 @@ https://www.youtube.com/watch?v=g0GgtqlhHaw&list=PLDyvV36pndZFHXjXuwA_NywNrVQO0a
 # ===============================================================================================
 # РЕКОМЕНДАЦИИ
 # ===============================================================================================
-
-
 # заголовок коммита должен быть не более 50 символов, далее может быть строка отступа и описание
 # далее примеры хороших заголовков
+
 feat: added new feature sorting on somepage.html
-test: added test of class UserModel
+test: added test class UserModel
 build(routes-for-api-docs): initial version
 refactor(routes): rename url '/pages/<int:id>'
 fix: validate 'parameter[].property' values
@@ -25,7 +24,7 @@ fix: validate 'parameter[].property' values
 # https://www.youtube.com/watch?v=3HJoXpC9vAM
 
 
-# для старта
+# старт
 git init                                  # создает git в текущей папке
 git branch -M main                        # создает ветку main
 git remote add origin git@github.com:VadimVolynkin/SomeRepo.git  # создает привязку к origin
@@ -58,44 +57,10 @@ git checkout -b new_branch2               # создаст новую ветку
 git checkout new_branch                   # переключаемся на послений коммит в new_branch(изменения текущей ветки могут быть перенесены)
 git branch -D new_branch                  # удаляем ветку
 
-git merge develop                         # обновляет текущую ветку коммитами из develop 
-git merge origin/master                   # слить ветки ветка с изменениями/куда
+git merge develop                         # слить в текущую ветку develop 
+git merge origin/master                   # слить в текущую ветку origin/master 
 
 git branch -v                             # покажет последний коммит в текущей ветке
-
-
-# === получение изменений из удаленного репо
-git fetch origin                          # получить изменения из удаленного репо origin
-git pull                                  # git fetch + git merge одной командой
-git pull origin main                      # явное указание откуда брать
-
-
-# === откат после commit
-git reset --hard HEAD^1                   # возврат на 1 коммит, все изменения удалены
-git reset ‐‐soft HEAD^1                   # возврат на 1 коммит + оставить все текущие изменения в stage
-
-# === очистка директории
-git clean -dxf                            # удаляет из директории только неотслеживаемые файлы, d - директории, x - файлы из .gitignore 
-
-# === удаление файлов из stage(после add)
-git reset .                               # удаляет из stage все файлы
-git reset index.html                      # удаляет из stage файл
-
-# === отмена внесенных в файлы изменений до stage(до add)
-git checkout -- .                         # отмена всех изменений
-git checkout -- index.html                # отмена изменений в файле
-git checkout -f HEAD                      # отмена всех изменений
-git checkout -f                           # отмена всех изменений
-
-# === восстановление файла 
-git checkout hash index.html              # до состояния на момент коммита
-git checkout HEAD index.html              # до состояния текущего коммита    
-git checkout index.html                   # до состояния текущего коммита из индекса в рабочую директорию    
-
-
-# === stash(позволяет отложить изменения, а потом снова вернуть)
-git stash                                 # соберет незакоммиченные изменения, удаляет их из файлов, и архивирует в гит 
-git stash pop                             # вернет изменения из stash в эту ветку или другую ветку(возможна ошибка)
 
 
 # === перенос коммитов из master в ветку fix
@@ -110,11 +75,92 @@ git branch master -f hash_of_right_master_commit
 git checkout -B master hash_of_right_master_commit
 
 
-# === reflog(история ссылок)
+# === получение изменений из удаленного репо
+git fetch origin                          # получить изменения из удаленного репо origin
+git pull                                  # git fetch + git merge одной командой
+git pull origin main                      # явное указание откуда брать
+
+
+# === ADD(добавление в индекс)
+git add .
+git add index.html
+git add -p index.html
+
+
+# === COMMIT(добавление в репозиторий)
+git commit -m "some message"
+git commit -am "some message"             # = git add + git commit
+git commit -c "some message"              # сделать коммит с уже имеющимся сообщением
+git commit --amend -m "some message"      # внесет изменения в последний коммит(пересоздаст его) + новый коммент
+git commit --amend --reset-author         # внесет изменения в последний коммит(пересоздаст его) + запишет текущего автора
+
+
+# === RESET откат после commit
+git reset --hard HEAD^1                   # возврат на 1 коммит, текущий stage удален, рабочая директория сброшена
+git reset ‐‐soft HEAD^1                   # возврат на 1 коммит, текущий stage останется, рабочая директория остается
+git reset ‐‐soft hash                     # перемещает ветку на нужный коммит, текущий stage останется, рабочая директория остается
+git reset --mixed HEAD^1                  # возврат на 1 коммит, текущий stage удален, рабочая директория остается
+git reset                                 # по умолчанию --mixed, в данном случае очистит от stage текущий коммит
+git reset index.html                      # удаляет из stage файл
+
+
+# === CLEAN(очистка директории)
+git clean -dxf                            # удаляет из директории только неотслеживаемые файлы, d - директории, x - файлы из .gitignore 
+
+
+# === отмена внесенных в файлы изменений до stage(до add)
+git checkout -- .                         # отмена всех изменений
+git checkout -- index.html                # отмена изменений в файле
+git checkout -f HEAD                      # отмена всех изменений
+git checkout -f                           # отмена всех изменений
+
+
+# === восстановление файла 
+git checkout hash index.html              # до состояния на момент коммита
+git checkout HEAD index.html              # до состояния текущего коммита    
+git checkout index.html                   # до состояния текущего коммита из индекса в рабочую директорию    
+
+
+# === STASH(позволяет отложить изменения, а потом снова вернуть)
+git stash                                 # соберет незакоммиченные изменения, удаляет их из файлов, и архивирует в гит 
+git stash pop                             # вернет изменения из stash в эту ветку или другую ветку(возможна ошибка)
+
+
+# === REFLOG(история ссылок)
 git reflog
 git reflog master
+git reflog feature -1                     # покажет посследнюю запись
 git reflog --date=iso                     # покажет ссылки с датами
 
+
+# === LOG
+git log                                   # выведет лог текущей ветки
+git log master                            # выведет лог ветки master
+git log master --online                   # выведет лог ветки master кратко
+git log feature ^master                   # покажет коммиты feature кроме коммитов доступных из ветки master
+git log master..feature                   # то же самое
+git log index.html                        # покажет коммиты в которых менялся файл
+git log -p index.html                     # покажет коммиты в которых менялся файл + различия
+git log --grep Run -i feature             # покажет все коммиты ветки feature со словом Run в заголовке (-i - регистронезависимость)
+git log --author=Vadim                    # покажет все коммиты ветки с автором
+git log --committer=Vadim                 # покажет все коммиты ветки с коммиттером
+git log --before '2018-02-23'             # покажет все коммиты до
+git log --after '2018-02-23 08:30:00 +02' # покажет все коммиты после
+
+
+# === DIFF
+git diff hash1 hash2                      # сравнивает состояние на момент 2 коммитов
+git diff master feature                   # сравнивает состояние на момент последних коммитов в ветках
+git diff master...feature                 # что именно изменилось в feature после отхождения от master
+git diff HEAD                             # сравнивает рабочую директорию с коммитом из HEAD
+git diff                                  # сравнивает рабочую директорию с индексом(покажет изменения не в индексе)
+git diff --cached                         # сравнивает индекс с последним коммитом(покажет что попадетв следующий коммит)
+git diff --no-index file1.py file2.py     # сравнивает любые 2 файла на диске независимо от наличия git
+git diff HEAD^1                           # родитель 1 у merge commit
+git diff HEAD^2                           # родитель 2 у merge commit
+git diff HEAD^1^                          # первый родитель первого родителя у merge commit
+git diff HEAD^2^                          # первый родитель второго родителя у merge commit
+git diff HEAD~3                           # переход по первому родителю на 3 коммита вниз(те по ветке вниз)
 
 
 # работа с тегами
@@ -125,42 +171,30 @@ git push origin v2.0                      # запушить тег
 git push origin :refs/tags/<tag-name>     # remove tag
 
 
-
-git pull ‐‐rebase
-git log ‐‐no-merges                       # list commit without merge and conflicts commit - only changes
-git revert ‐‐no-commit [commit]
-git diff -w                               # watch changes between 2 commits
-git log 2                                 # смотреть последние 2 коммита
-
-
-https://github.com/k88hudson/git-flight-rules/blob/master/README_ru.md
-git show                 последний коммит в HEAD 
-git log -n1 -p
-
-
-git show <commitid>:filename
-
-git commit --amend --only -m 'xxxxxxx'
-
-
-
-
-
-
-
-
-
-
-
 # === MERGE(2 ветки сливаются в 1)
 # merge не меняет историю, поэтому подходит для работы над feature1 веткой сразу нескольих разработчиков
 
-# все изменения попадут в мастер в виде 1 коммита
-git merge develop                         # обновляет текущую ветку коммитом из develop
-git merge develop feature1                # обновляет develop коммитом из feature1
+# все коммиты develop попадут в мастер(через слияние перемоткой)
+git merge develop                         # обновляет текущую ветку master коммитами из develop
+git merge master develop                  # обновляет master коммитами из develop
+
+# мержит через создание коммита слияния(исключает мерж через перемотку)
+# все коммиты ветки feature остаются в ней
+# коммит слияния содержит в себе все коммиты feature
+git merge --no-ff --no-edit feature
+git config merge.ff false                           # включает --no-ff во все коммиты по умолчанию
+git config branch.master.mergeoptions '--no-ff'     # включает --no-ff для конкретной ветки по умолчанию
 
 # мержит в текущую ветку новый коммит из удаленного репо origin/develop
-git merge origin/master                   
+git merge origin/master      
+
+# выполнит мерж перенеся только последний коммит из fix
+# промежуточные коммиты ветки fix не будут включены 
+# при использовании --squash файл MERGE_HEAD не создается
+git merge --squash fix
+
+# отмена слияния сделанного только что
+git reset --hard ORIG_HEAD
 
 
 # === REBASE(добавляет новые коммиты к старым)
@@ -168,7 +202,18 @@ git merge origin/master
 # rebase удобно использовать для обновления своей feature1 ветки из develop
 # с rebase опасно работать если с feature1 веткой работает сразу несколько разработчиков
 # все изменения из feature1 будут добавлены после всех коммитов develop
-git rebase develop
+git rebase develop                             # перебазирует develop на вершину текущей ветки
+git rebase develop feature                     # перебазирует feature на вершину develop
+
+# перебазирование части ветки на мастер
+# от ветки master отходит ветка feature, а от нее fix
+git rebase --onto master feature               # перебазирует коммиты от feature до fix
+
+# запуск комманды после каждого перебазированного коммита(обычно это тесты)
+git rebase -x 'run tests' master               # в случае падения тестов перебазирование остановится для исправления
+
+# отмена изменений rebase
+git reset --hard ORIG_HEAD
 
 # обновляет текущую ветку из удаленного репо origin/develop 
 git pull --rebase origin/develop          
@@ -177,29 +222,44 @@ git pull --rebase origin/develop
 git rebase -i feature1                     # интерактивный rebase  на feature1
 
 # интерактивный rebase текущей ветки чтобы изменить историю
-git rebase -i HEAD~3                       # редактирование второго коммита с конца    
-# edit 4b6e19a The second to last commit   # edit указывает что этот коммит будем менять, остальные оставим
-# pick f4037ec The last commit
+git rebase -i HEAD~3                       # редактирование второго коммита с конца  
 # --- popular options
-# pick    # без изменений
-# reword  # без изменений + изменение commit message
-# fixup   # изменения этого коммита будут записаны в предыдущий коммит(как amend). Сам коммит исчезнет.
-# drop    # удалит коммит
+# pick f4037ec The last commit             # копирование без изменений 
+# reword                                   # копирование без изменений + редактируем commit message
+# edit 4b6e19a The second to last commit   # скопирует коммит и позволит отредактировать(сделать amend)
+# quash                                    # коммит нужно слить с предыдущим, сообщения объединить, коммит удалить
+# fixup                                    # коммит нужно слить с предыдущим, сообщения удалить, коммит удалить
+# exec                                     # добавление произвольных коммандпосле копирования коммита(например тесты)
+# drop                                     # удалит коммит
+
 git add .                                  # вносит изменения в stage
 git commit --amend                         # пересоздаст коммит с прежним сообщением
 git rebase --continue                      # продолжит изменения
 git push origin feature1 --force           # запушит в удаленную ветку feature1
 git push origin +feature1                  # тоже самое (запушит в удаленную ветку feature1)
 
+git rebase --abort                         # удалит информацию о процессе rebase, вернув все в исходное состояние
+git rebase --skip                          # пропустит коммит в случае конфликта
 
-# REVERT(новый коммит, который отменяет изменения в коммите с ошибкой. Реверт не меняет историю.)
+
+# сделает заплатку типа аменда после любого коммита
+git commit -a --fixup=32d44                # создаем коммит заплатку с исправлениями для коммита 32d44. Поставит коммит сразу после 32d44. 
+git rebase -i --autosquash                 # перебазирует ветку, плохой коммит и заплатка будут объединены.
+
+git config --global rebase.autoSquash true # включает autosquash по умолчанию для git rebase -i
+
+# === REVERT(новый коммит, который отменяет изменения в коммите с ошибкой(создает обратный коммит). Реверт не меняет историю.)
 git revert hash_badcommit
 
 
-# CHERRY PICK(переносит 1 конкретный коммит в выбранную ветку)
+# === CHERRY PICK(копирует 1 конкретный коммит в выбранную ветку)
 git cherry-pick hash
 git cherry-pick hash --edit        # выполнит команду с старым commit message
-git cherry-pick hash --no-commit   # перенесет коммит в целевую ветку, но не создаст там коммит, а даст возможность доработать
+git cherry-pick hash --no-commit   # скопирует коммит в целевую ветку, но не создаст там коммит, а даст возможность доработать
+git cherry-pick --abort            # отменит коммит, вернет как было
+git cherry-pick --continue         # продолжит выполнение cherry-pick
+git cherry-pick --quit             # остановиться сейчас и сбросить полученное состояние
+
 
 # === Global Setting
 git config --global user.name "Vadim Volynkin"
@@ -207,11 +267,11 @@ git config --global user.email "some@mail.ru"
 git config -l
 
 
-
 # ===============================================================================================
 # GIT ALIAS
 # ===============================================================================================
 # ~/.gitconfig
+
 
 [alias]
     a = add
@@ -225,6 +285,7 @@ git config -l
     ds = diff --staged
     extend = commit --amend -C HEAD
     f = fetch
+    l = log --format=format:'%C(yellow)%h %C(green)%ad %C(dim white)[%an] %C(dim cyan)%s%C(reset)%C(bold blue)%d' --date=format:'%F %R'
     loll = log --graph --decorate --pretty=oneline --abbrev-commit
     g =  log --graph --abbrev-commit --decorate --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)'
     m = merge
@@ -238,8 +299,6 @@ git config -l
     zap = fetch -p
     day = log --reverse --no-merges --branches=* --date=local --since=midnight --author=\"$(git config --get user.name)\"
     delete-merged-branches = "!f() { git checkout --quiet main && git branch --merged | grep --invert-match '\\*' | xargs -n 1 git branch --delete; git checkout --quiet @{-1}; }; f"
-
-
 
 
 # ===============================================================================================
